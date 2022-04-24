f4aa4a33b67bec6f7179d71be7e18656bd64fb77
Gilberth Renteria <gilberthr10@gmail.com>
2022-04-23 20:41:19 -0500

    initial commit -m

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'] 
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/assets/black-square.svg')",
+      }
+    },
+  },
+  plugins: [],
+}
